63f592c2f422c887d6161322259d0ed6c38c6c0f
huohua51 <167312988+huohua51@users.noreply.github.com>
2025-07-02 20:24:57 +0800

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-﻿from django.urls import path
-from .views import LoginView, TestConnectionView
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('test/', TestConnectionView.as_view(), name='test'),
-]
